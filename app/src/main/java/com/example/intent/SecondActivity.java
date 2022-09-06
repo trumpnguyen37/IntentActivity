0c044ec651fdf8a1b62061bc6487692a57cc399b
trumpnguyen37 <vantrung03072002@gmail.com>
2022-09-06 08:44:24 +0700

    First commit

---
+++ app/src/main/java/com/example/intent/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    Button btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        btnBack = (Button) findViewById(R.id.buttonBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
